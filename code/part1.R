96bd352ef4b17ed2cca7e18049724afd7ada38f1
quochuy201 <quochuy201@yahoo.com>
2018-11-30 11:04:32 -0800

    put some comments on code

--- code/part1.R
+++ code/part1.R
@@ -6,10 +6,10 @@ library(GGally)
 library("gridExtra")
 library("here")
 #
-getwd()
+here()
 
 #load data
-murders_df<-read.csv("C:/Users/quoch/OneDrive/Documents/R/ECON5100/Problemset-4/raw_data/murders.csv")
+murders_df<-read_csv(here::here("raw_data","murders.csv"))
 murders_df <- filter(murders_df, year=='1995')
 
 #question 1
@@ -22,22 +22,23 @@ q2<- select(murders_df,murders, popul,arrests,murdrate, arrestrate,percblack)
 cor_coeff <- cor(q2, method='pearson')
 
 #question 3 & 4
+# first model:
+# murders= %10~19 + %20~29 +  %male + %black + %income
 names(murders_df)
 lm_murd_q3<- lm(murders ~ perc1019+ perc2029 + percblack + percmale + rpcpersinc ,murders_df)
 summary(lm_murd_q3)
 
 #question 5
-murder_stdres<-rstandard(lm_murd_q3)
 q5_df<- select(murders_df,murders, perc1019, perc2029 , percblack, percmale, rpcpersinc)
 q5_df<- mutate(q5_df,"predicted"= predict(lm_murd_q3),
                "residuals"=residuals(lm_murd_q3),
                "stand_res" = rstandard(lm_murd_q3)
-               )
-summary(q5_test)
+               ) # add column for fitted value, residuals, standardize residual
 
-# residual vesus fitted value
-q5_df %>% ggplot(aes(x = predicted, y = rediduals)) + geom_point() +
-  geom_smooth(method = "lm", se = F)+
+
+# residual vesus fitted value graph
+q5_df %>% ggplot(aes(x = predicted, y = residuals)) + geom_point() +
+  geom_smooth(method = "lm", se = F)
   ggsave(here("figures", "Part1_diag_pred_resd.png"))
 
 # Standardised residuals histogram
--- code/part2.R
+++ code/part2.R
@@ -1,6 +1,7 @@
 #Part 2:
 
 #lm model for question 6:
+# murders rate = %10~19 + %20~29 +  %male + %black + %income
 lm_murdrate_q6<-lm(murdrate~perc1019+ perc2029 + percblack + percmale + rpcpersinc ,murders_df)
 summary(lm_murdrate_q6)
 
@@ -8,14 +9,12 @@ q6_df<-select(murders_df,murdrate, perc1019, perc2029 , percblack, percmale, rpc
 
 q6_df<- q6_df%>%mutate("predicted"= predict(lm_murdrate_q6), "residuals"=residuals(lm_murdrate_q6),
                        "stand_res" = rstandard(lm_murdrate_q6))
-# residual vesus fitted value
+
+# residual vesus fitted value plot
 q6_df %>% ggplot(aes(x = predicted, y = residuals)) + geom_point() +
   geom_smooth(method = "lm", se = F)
-  #ggsave(here("figures", "Part2_diag_pred_resd.png"))
+ggsave(here("figures", "Part2_diag_pred_resd.png"))
 
-q6_df %>% ggplot(aes(x = predicted, y = residuals)) +
-  geom_point()
-ggsave(here("figures", "diag_nw_homoskedasticity.png"))
 
 # Standardised residuals histogram
 q6_df %>% ggplot(aes(x = stand_res)) +
